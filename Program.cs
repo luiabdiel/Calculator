2be4a15f58c811d46e6a723bbd6aaed9cbe09986
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-01-24 22:57:17 -0300

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
